641f753db38a0da3854e5613e60d476a25ff584c
Tommyshadex <anikettomar19@gmail.com>
2022-01-19 00:29:13 +0530

    ADDED button to svg

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded',() =>{
+    const Layer1 =document.getElementById('Layer_1');
+
+   const move = function(){
+       //Layer1.classList.contains('Marking')
+       Layer1.classList.add('Marking');
+   }
+        Layer1.addEventListener('click',move)
+    
+    }
+
+
+
+
+
+    
+)
