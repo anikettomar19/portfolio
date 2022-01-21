f459d211353ed3cd04968aa1c603390a1b2477cb
Tommyshadex <anikettomar19@gmail.com>
2022-01-22 00:04:19 +0530

    Index page completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,21 @@ const btn = document.querySelector("[data-btn]")
 btn.addEventListener("click", () => {
   btn.classList.toggle("animating")
 })
-const tl = new TimelineMax();
+     // Select the button
+     const button = document.querySelector('.btn-toggle');
+
+     // Listen for a click on the button
+     button.addEventListener('click', function() {
+       // Then toggle (add/remove) the .dark-theme class to the body
+       document.body.classList.toggle('dark-theme');  
+     })
+     const dark = document.getElementById("dark-theme");
+
+     dark.addEventListener("click", ()=>{
+     
+         if(dark.innerText === "Light Mode"){
+             dark.innerText = "Dark Mode";
+         }else{
+             dark.innerText= "Light Mode";
+         }
+     });
